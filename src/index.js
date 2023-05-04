47b02b62ce0b68b7a516d9ae40a257a839fbedf6
ivanbatarin <ivan.batarin@boosta.co>
2023-05-04 12:01:16 +0200

    fixing v1

--- src/index.js
+++ src/index.js
@@ -16,15 +16,55 @@ document.addEventListener('DOMContentLoaded', () => {
    //    })
    // }
 
-   const swiper = new Swiper('.swiper', {
-      modules: [Pagination],
-      speed: 1000,
-      slidesPerView: "auto",
-      spaceBetween: 8,
 
-      pagination: {
-         el: ".swiper-pagination",
-         dynamicBullets: true,
+   document.addEventListener("scroll", (event) => {
+
+      let header = document.querySelector('.header');
+      if (window.scrollY > 0) {
+         header.classList.add('white-js');
+      }else {
+         header.classList.remove('white-js');
       }
-    });
+
+   });
+
+   if(window.innerWidth < 768) {
+
+      const swiper = new Swiper('.swiper-js-reviews', {
+         modules: [Pagination],
+         speed: 1000,
+         slidesPerView: "auto",
+         spaceBetween: 8,
+
+         pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+         }
+      });
+   }
+
+   document.addEventListener("resize", (event) => {
+
+      let swiper = document.querySelector('.swiper-js-reviews');
+      let swiperWrapper = swiper.querySelector('.swiper-wrapper');
+
+      if(window.innerWidth > 768) {
+         swiper.destroy();
+      } else {
+         const swiper = new Swiper('.swiper-js-reviews', {
+            modules: [Pagination],
+            speed: 1000,
+            slidesPerView: "auto",
+            spaceBetween: 8,
+
+            pagination: {
+               el: ".swiper-pagination",
+               dynamicBullets: true,
+            }
+         });
+
+      }
+
+   });
+
 });
