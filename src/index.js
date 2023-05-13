f4609496f8cf5f253f516744a10c664d8f431b9f
hamriyat <vladlahman@gmail.com>
2023-05-13 13:36:48 +0300

    making it responsive

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import './style.scss';
-
 import Swiper, { Pagination } from 'swiper';
-import lozad from 'lozad'
+import lozad from 'lozad';
 
 document.addEventListener('DOMContentLoaded', () => {
-
 
    const observer = lozad();
    observer.observe();
 
    // burger menu
-   // const menuBtn = document.querySelector('.nav__burger');
-   // const menuBody  = document.querySelector('.menu');
+   const menuBtn = document.querySelector('.nav__burger');
+   const menuBody  = document.querySelector('.menu');
 
-   // if (menuBtn && menuBody) {
-   //    menuBtn.addEventListener('click', function (e) {
-   //       document.body.classList.toggle('scroll-lock-js');
-   //       menuBtn.classList.toggle('nav__burger--active-js');
-   //       menuBody.classList.toggle('menu--active-js');
-   //    })
-   // }
+   if (menuBtn && menuBody) {
+      menuBtn.addEventListener('click', function (e) {
+         document.body.classList.toggle('scroll-lock-js');
+         menuBtn.classList.toggle('nav__burger--active-js');
+         menuBody.classList.toggle('menu--active-js');
+      })
+   }
 
 
    const top5Swiper = new Swiper('.top-5__swiper', {
@@ -57,38 +55,82 @@ document.addEventListener('DOMContentLoaded', () => {
    let categories = false;
 
    changeSwiperDirection ();
+   categoriesSwiper();
+
 
    window.addEventListener('resize', () => {
       changeSwiperDirection();
+      categoriesSwiper();
    })
 
 
-   function changeSwiperDirection () {
-      if (window.innerWidth < 1150) {
-         guestbookSwiper.changeDirection('horizontal');
-
+   function categoriesSwiper () {
+      if (window.innerWidth < 650) {
          categories = new Swiper('.categories__body', {
             modules: [Pagination],
             speed: 1000,
-            slidesPerView: "auto",
+            slidesPerView: 1,
             spaceBetween: 8,
       
             pagination: {
                el: ".swiper-pagination",
                dynamicBullets: true,
-            }
+            },
          });
          
       } else {
-
-         guestbookSwiper.changeDirection('vertical');
-
          if (categories) {
             categories.destroy(true, true);
             categories = false;
          }
+      }
+   }
 
+   function changeSwiperDirection () {
+      if (window.innerWidth < 1150) {
+         guestbookSwiper.changeDirection('horizontal');
+         
+      } else {
+         guestbookSwiper.changeDirection('vertical');
       }
    }
    
+
+   // navigation scroll to
+const navLink = document.querySelector('.hero__link');
+
+if (navLink) {
+   navLink.addEventListener("click", (e) => {
+      let gotoBlock = document.querySelector('#categories');
+      if (gotoBlock) {
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+         window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+         });
+      }
+      e.preventDefault();
+   });
+
+}
+
+// white header on scroll
+
+document.addEventListener("scroll", (event) => {
+
+   let header = document.querySelector('.header');
+   if (window.scrollY > 0) {
+      header.classList.add('header-white-js');
+   } else {
+      header.classList.remove('header-white-js');
+   } 
 });
+      
+   
+
+});
+
+
+
+      
+   
